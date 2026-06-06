ad5caee3587e47a226ec611a6e1c396499e455ab
Bhavya Srivastava <111657208+BhavyaSrvstv@users.noreply.github.com>
2026-06-06 15:14:10 +0530

    Add files via upload

---
+++ Assignments/Assignment_2/BhavyaSrivastava_01104092025/1_Sum_Natural.py
@@ -0,0 +1,7 @@
+# Sum of first 10 natural numbers
+
+sum = 0
+for i in range(1, 11):
+    sum += i
+
+print("Sum of first 10 natural numbers =", sum)
---
+++ Assignments/Assignment_2/BhavyaSrivastava_01104092025/2_Factorial.py
@@ -0,0 +1,9 @@
+# Factorial of a number
+
+num = int(input("Enter a number: "))
+factorial = 1
+
+for i in range(1, num + 1):
+    factorial *= i
+
+print("Factorial of", num, "=", factorial)
